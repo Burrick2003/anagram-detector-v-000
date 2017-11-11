b29982ca0b14ecc369ba3ab8745576aee908fc71
Blakeley Bond <burrick2003@yahoo.com>
2017-11-11 02:02:45 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,25 @@
 # Your code goes here!
+Class Anagram
+attr_accessor :word_input, :match_input
+
+  def initialize(input)
+    @word_input = input
+
+  end
+
+
+
+  def match(input)
+    @match_input = input
+      @match_input.select do |string|
+        @word_input.split("") == @match_input.split("")
+      end
+    end
+
+  end
+
+
+
+
+
+end
