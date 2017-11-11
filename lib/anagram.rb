f16a2b39bc52242536502affc2f271a1d8dc131f
Blakeley Bond <burrick2003@yahoo.com>
2017-11-11 02:35:37 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -9,11 +9,6 @@ attr_accessor :word_input, :match_input
 
   def match(array)
       array.select {|x| x.split("").sort == @name.split("").sort}
-    end   
-  def match2(input)
-    @match_input = input
-      @match_input.select do |string|
-        input.split("").sort == string.split("").sort
-      end
-  end
+    end
+
 end
