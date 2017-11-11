734c546da95a43d8a8ae2bef94785774cbddcc0d
Blakeley Bond <burrick2003@yahoo.com>
2017-11-11 02:40:28 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,13 +7,10 @@ class Anagram
   end
 
 def match(input)
-  input.select do |element|
+  @match_input = input
+  @match_input.select do |element|
     @word_input.split("").sort == element.split("").sort
   end
 end
-
-  def match2(array)
-    array.select {|x| x.split("").sort == @name.split("").sort}
-  end
 
 end
